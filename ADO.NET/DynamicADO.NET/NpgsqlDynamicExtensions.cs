9cdea08ba845c48f8b37f0084b010332ea04476f
Ilshat Aliyev <aliev.ilshat@gmail.com>
2014-01-01 00:57:44 +0100

    Initial Commit

---
+++ ADO.NET/DynamicADO.NET/NpgsqlDynamicExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Npgsql;
+
+namespace DynamicTest
+{
+    public static class NpgsqlDynamicExtensions
+    {
+        public static dynamic CreateDynamicCommand(this NpgsqlConnection con) 
+        {
+            return new DynamicCommandProvider(con);
+        }
+    }
+}
---
+++ ADO.NET/DynamicADO.NET/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace DynamicTest
+{
+    class Program
+    {
+        const string CONNECTION_STRING = "User ID=postgres;Password=qazqaz;Host=192.168.254.60;Port=5432;Database=naiton3";
+
+        static void Main(string[] args)
+        {
+            using (var con = new NpgsqlConnection(CONNECTION_STRING))
+            {
+                con.Open();
+                getdata(con);
+                Console.WriteLine("------------------------------------------------------------");
+                getdatadyn(con);
+            }
+        }
+
+        private static void getdatadyn(NpgsqlConnection con)
+        {
+            dynamic cmd = con.CreateDynamicCommand();
+            var res = cmd.ordermanager_getorderdetailsbyid(_orderid: 186, _cousevalue: 1).ExecuteReader();
+
+            foreach (var i in res)
+            {
+                Console.WriteLine(i.name);
+            }
+        }
+
+        private static void getdata(NpgsqlConnection con)
+        {
+            var transaction = con.BeginTransaction();
+            var cmd = con.CreateCommand();
+            cmd.CommandText = "ordermanager_getorderdetailsbyid";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("_orderid", 186);
+            cmd.Parameters.AddWithValue("_coursevalue", 1);
+            var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["name"]);
+            }
+            transaction.Commit();
+        }
+    }
+}
